765ee85e4cc50304bc7953e47c147fd5ab7a3933
Leonard Truong <leonardtruong@gmail.com>
2015-04-04 16:31:25 -0700

    Initial UI Implemented

--- PECS/ViewController.swift
+++ PECS/ViewController.swift
@@ -20,6 +20,31 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBOutlet weak var heaterBackSlider: UISlider!
+    @IBOutlet weak var heaterBackLabel: UILabel!
+    @IBAction func heaterBackSliderChanged(sender: AnyObject) {
+        self.heaterBackLabel.text = "\(Int(self.heaterBackSlider.value))"
+    }
 
+    @IBOutlet weak var heaterBottomLabel: UILabel!
+    @IBAction func heaterBottomSliderChanged(sender: UISlider) {
+        self.heaterBottomLabel.text = "\(Int(sender.value))"
+    }
+    
+    @IBOutlet weak var fanBackLabel: UILabel!
+    @IBAction func fanBackSliderChanged(sender: UISlider) {
+        self.fanBackLabel.text = "\(Int(sender.value))"
+    }
+    @IBOutlet weak var fanBottomLabel: UILabel!
+    @IBAction func fanBottomSliderChanged(sender: UISlider) {
+        self.fanBottomLabel.text = "\(Int(sender.value))"
+    }
+    @IBAction func seatedSwitchClicked(sender: UISwitch) {
+        if sender.on {
+            sender.setOn(false, animated:true)
+        } else {
+            sender.setOn(true, animated:true)
+        }
+    }
 }
 
