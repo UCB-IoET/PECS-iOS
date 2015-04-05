97bdb494c624e7f4c57ddc6c8bb6d71d8ed63969
Leonard Truong <leonardtruong@gmail.com>
2015-04-04 17:15:11 -0700

    Store settings and send updates.

--- PECS/ViewController.swift
+++ PECS/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import Alamofire
+
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.restoreChairState()
         // Do any additional setup after loading the view, typically from a nib.
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "saveSwitchesStates", name: "kSaveChairState", object: nil);
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,24 +32,65 @@ class ViewController: UIViewController {
     }
 
     @IBOutlet weak var heaterBottomLabel: UILabel!
+    @IBOutlet weak var heaterBottomSlider: UISlider!
     @IBAction func heaterBottomSliderChanged(sender: UISlider) {
         self.heaterBottomLabel.text = "\(Int(sender.value))"
     }
     
     @IBOutlet weak var fanBackLabel: UILabel!
+    @IBOutlet weak var fanBackSlider: UISlider!
     @IBAction func fanBackSliderChanged(sender: UISlider) {
         self.fanBackLabel.text = "\(Int(sender.value))"
     }
     @IBOutlet weak var fanBottomLabel: UILabel!
+    @IBOutlet weak var fanBottomSlider: UISlider!
     @IBAction func fanBottomSliderChanged(sender: UISlider) {
         self.fanBottomLabel.text = "\(Int(sender.value))"
     }
+    @IBOutlet weak var seatedSwitch: UISwitch!
     @IBAction func seatedSwitchClicked(sender: UISwitch) {
         if sender.on {
             sender.setOn(false, animated:true)
         } else {
             sender.setOn(true, animated:true)
         }
+    }
+    @IBAction func stateDidChange(sender: AnyObject) {
+        let parameters: [String: AnyObject] = [
+            "backFan": Int(self.fanBackSlider.value),
+            "bottomFan": Int(self.fanBottomSlider.value),
+            "backHeater": Int(self.heaterBackSlider.value),
+            "bottomHeater": Int(self.heaterBottomSlider.value),
+            "occupancy": self.seatedSwitch!.on
+        ]
+        
+        Alamofire.request(.POST, "http://shell.storm.pm:38027", parameters: parameters, encoding: .JSON)
+    }
+    
+    func saveChairState() {
+        NSUserDefaults.standardUserDefaults().setInteger(Int(self.heaterBackSlider.value), forKey: "heaterBackSlider")
+        NSUserDefaults.standardUserDefaults().setInteger(Int(self.heaterBottomSlider.value), forKey: "heaterBottomSlider")
+        NSUserDefaults.standardUserDefaults().setInteger(Int(self.fanBackSlider.value), forKey: "fanBackSlider")
+        NSUserDefaults.standardUserDefaults().setInteger(Int(self.fanBottomSlider.value), forKey: "fanBottomSlider")
+        NSUserDefaults.standardUserDefaults().setInteger(Int(self.fanBottomSlider.value), forKey: "fanBottomSlider")
+        NSUserDefaults.standardUserDefaults().setBool(self.seatedSwitch!.on, forKey: "seated")
+    }
+    func restoreChairState() {
+        self.seatedSwitch!.on = NSUserDefaults.standardUserDefaults().boolForKey("seated")
+        
+        var fanBottom = NSUserDefaults.standardUserDefaults().integerForKey("fanBottomSlider")
+        self.fanBottomLabel.text = "\(fanBottom)"
+        self.fanBottomSlider.value = Float(fanBottom)
+        var fanBack = NSUserDefaults.standardUserDefaults().integerForKey("fanBackSlider")
+        self.fanBackLabel.text = "\(fanBack)"
+        self.fanBackSlider.value = Float(fanBottom)
+        
+        var heaterBottom = NSUserDefaults.standardUserDefaults().integerForKey("heaterBottomSlider")
+        self.heaterBottomLabel.text = "\(heaterBottom)"
+        self.heaterBottomSlider.value = Float(heaterBottom)
+        var heaterBack = NSUserDefaults.standardUserDefaults().integerForKey("heaterBackSlider")
+        self.heaterBackLabel.text = "\(heaterBack)"
+        self.heaterBackSlider.value = Float(heaterBack)
     }
 }
 
